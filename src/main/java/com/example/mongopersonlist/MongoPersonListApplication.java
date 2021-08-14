384bed3f14d2a315bed04d0cc59cac579f10f5c4
Valeriy Emelyanov <emelva@rambler.ru>
2021-08-14 16:17:30 +0300

    Start

---
+++ src/main/java/com/example/mongopersonlist/MongoPersonListApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongopersonlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoPersonListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoPersonListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mongopersonlist/MongoPersonListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongopersonlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoPersonListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
